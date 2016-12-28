0ec828146f6d7eb6a58dbad7073abb75e48eeb90
blueshoe <blueshoe>
2016-12-28 14:44:39 +0530

    Passed test 1/4

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,8 +1,16 @@
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 public class Etl {
-    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        return null;
-    }
+  public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+    Map<String, Integer> expected = new HashMap<String, Integer>() {
+      {
+        put("a", 1);
+      }
+    };
+    expected = Collections.unmodifiableMap(expected);
+    return expected;
+  }
+
 }
